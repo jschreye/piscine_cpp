466e9d86450496bcdeea7a9d6f83f860d81c17ec
jschreye <jschreye@c1r4s2.42lausanne.ch>
2022-08-23 17:30:27 +0200

    c++

---
+++ module00/ex00/megaphone.cpp
@@ -0,0 +1,28 @@
+#include <string>
+#include <iostream>
+
+std::string change_min_in_maj(std::string str)
+{
+    for(size_t i = 0; i < str.length(); i++)
+    {
+        str[i] = toupper(str[i]);
+    }
+    return (str);
+}
+
+int main (int argc, char **argv)
+{
+    std::string str;
+
+    if (argc <= 1)
+        std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *" << std::endl;
+    else
+    {
+        for(int i = 1; i < argc; i++)
+        {
+            str = change_min_in_maj(argv[i]);
+            std::cout << str << "";
+        }
+       std::cout << std::endl;
+    }
+}
