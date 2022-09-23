44818857c004b487a6446733848f7dfb5a3bce5d
Jérémie Schreyer <jeremieschreyer@gmail.com>
2022-09-23 10:00:20 +0200

    fini cette fois

--- module07/ex02/main.cpp
+++ module07/ex02/main.cpp
@@ -1,53 +1,35 @@
 #include <iostream>
 #include "Array.hpp"
 
-#define MAX_VAL 750
-int main(int, char**)
+#define TAB_SIZE 15
+int main(void)
 {
-    Array<int> numbers(MAX_VAL);
-    int* mirror = new int[MAX_VAL];
-    srand(time(NULL));
-    for (int i = 0; i < MAX_VAL; i++)
-    {
-        const int value = rand();
-        numbers[i] = value;
-        mirror[i] = value;
-    }
-    //SCOPE
-    {
-        Array<int> tmp = numbers;
-        Array<int> test(tmp);
-    }
+	Array<int> a(TAB_SIZE);
 
-    for (int i = 0; i < MAX_VAL; i++)
-    {
-        if (mirror[i] != numbers[i])
-        {
-            std::cerr << "didn't save the same value!!" << std::endl;
-            return 1;
-        }
-    }
-    try
-    {
-        numbers[-2] = 0;
-    }
-    catch(const std::exception& e)
-    {
-        std::cerr << e.what() << '\n';
-    }
-    try
-    {
-        numbers[MAX_VAL] = 0;
-    }
-    catch(const std::exception& e)
-    {
-        std::cerr << e.what() << '\n';
-    }
+	try
+	{
+		for (size_t i = 0; i < TAB_SIZE; i++)
+			a[i] = i;
+		for (size_t i = 0; i < TAB_SIZE; i++)
+			std::cout << "a[" << i << "] " << a[i] << '\n';
+		std::cout << std::endl;
+	}
+	catch(const std::exception& e)
+	{
+		std::cerr << e.what() << '\n';
+	}
 
-    for (int i = 0; i < MAX_VAL; i++)
-    {
-        numbers[i] = rand();
-    }
-    delete [] mirror;//
-    return 0;
+	try
+	{
+		Array<int> b(a);
+
+		b[0] = 8;
+		std::cout << "b:" << b[0] << std::endl;
+		std::cout << "a:" << a[3] << std::endl;
+	}
+	catch(const std::exception& e)
+	{
+		std::cerr << e.what() << '\n';
+	}
+	return (0);
 }
